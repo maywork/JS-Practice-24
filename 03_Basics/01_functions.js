089b176233586669cd17789254429d9ba479a6e4
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2023-12-25 13:29:49 +0600

    added function notes

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -23,11 +23,49 @@ const result = addTwoNumbers(3, 5)
 
 
 function loginUserMessage(username) {
-  if (username === undefined) {
+  if (!username) {
     return ("Please enter a username")
   }
   return `${username} just logged in`
 }
 
-console.log(loginUserMessage("May"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage("May"))
+// console.log(loginUserMessage())
+
+
+function calculateCartPrice(...num1) {
+  return num1
+}
+console.log(calculateCartPrice(200, 400, 500));
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1
+}
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+
+const user = {
+  username: "mayesha",
+  price: 199
+  // prices: 199
+}
+
+function handleObject(anyobject) {
+  console.log(`Username is ${anyobject.username} & price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+  username: "sam",
+  price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+  return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100, 200, 500, 1000]));
