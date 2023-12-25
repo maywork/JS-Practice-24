ccbc799c5b67f2b4cc9ccaee011b471e78824583
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2023-12-25 22:03:18 +0600

    added scope level notes

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -14,7 +14,47 @@ console.log(a);
 // console.log(b);
 console.log(c); // should not have printed outside scope 
 
-for (let i = 0; i < array.length; i++) {
-  const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//   const element = array[i];
+// }
 
+
+function one() {
+  const username = "mayesha"
+
+  function two() {
+    const website = "youtube"
+    console.log(username);
+    return website
+  }
+  return two()
 }
+
+// console.log(one());
+
+if (true) {
+  const username = "Mayesha"
+  if (username === "Mayesha") {
+    const website = "youtube"
+
+    console.log(`${username} ${website}`);
+  }
+  // console.log(website); //outside scope
+}
+
+// console.log(username); //outside scope
+
+console.log(addone(10))
+
+function addone(num) {
+  return num + 1
+}
+
+console.log(addone(5))
+
+// addTwo(5) // Cannot access 'addTwo' before initialization
+const addTwo = function (num) {
+  return num + 2
+}
+
+console.log(addTwo(5))
