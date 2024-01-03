9c39ea68400508148130eeb9fe3194c324abc9e7
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-01-03 15:51:36 +0600

    added control flow notes

--- 04_Control_Flow/one.js
+++ 04_Control_Flow/one.js
@@ -37,8 +37,40 @@ if (temperature === 50) {
 }
 
 
-const score = 200
-if (score > 100) {
-  const power = 'fly'
-  console.log(`User power: ${power}`);
+// const score = 200
+// if (score > 100) {
+//   const power = 'fly'
+//   console.log(`User power: ${power}`);
+// }
+
+
+// shorthand notation
+
+// const balance = 1000
+// if (balance > 500) console.log("test"), // bad practice
+//   console.log("test 2"),
+//   console.log("test 3")
+
+// if (balance < 500) {
+//   console.log('less than 500');
+// } else if (balance < 750) {
+//   console.log('less than 750');
+// } else if (balance < 900) {
+//   console.log('less than 900');
+// } else {
+//   console.log('less than 1500');
+// }
+
+
+const userLoggedIn = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = false
+const debitCard = true
+
+if (userLoggedIn && debitCard) {
+  console.log("Allowed to buy courses");
+}
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+  console.log("User logged in");
 }
