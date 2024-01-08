9d5576dffa6634f34bb529561a81d2e19228c1c6
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-01-08 15:24:32 +0600

    added iteration notes part 2

---
+++ 05_Iterations/while_loop.js
@@ -0,0 +1,16 @@
+// while loop
+
+let index = 0
+while (index <= 10) {
+  console.log(`Value of index ${index}`);
+  index++
+}
+
+let myArray = ["batman", "flash", "ironman", "spiderman", "dr. strange"]
+
+let arr = 0
+
+while (arr < myArray.length) {
+  console.log(`value is ${myArray[arr]} index is ${arr}`);
+  arr++;
+}
