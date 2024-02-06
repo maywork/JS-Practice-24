619c43f4660274eca858cde643b55f356fc0a28f
Mayesha Hridula <mayesha.fairuz.work@gmail.com>
2024-02-06 22:07:37 +0600

    added older getters & setters notes

--- 10_OOP/getter_setter.js
+++ 10_OOP/getter_setter.js
@@ -23,3 +23,22 @@ class User {
 const mayesha = new User('m@yesha.com', '123abc')
 console.log(mayesha.email);
 console.log(mayesha.password);
+
+
+// older methods to set
+
+// get & set using Object
+// const User = {
+//   _email: 'm@mf.com',
+//   _password: 'abc',
+
+//   get email() {
+//     return this._email.toUpperCase()
+//   },
+//   set email(value) {
+//     this._email = value
+//   }
+// }
+
+// const tea = Object.create(User)
+// console.log(tea.email);
